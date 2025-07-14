8e2ce6d0771cfd4d81d7b9c1c972ac77682bbb40
DarkCaster <dark.caster@outlook.com>
2025-07-14 19:05:03 +0300

    Revert "Workflow: set GOHOSTARCH"
    This reverts commit 37c40a129ed035c7dcea541e5c42472b6b0aedec.

--- build.ps1
+++ build.ps1
@@ -42,6 +42,7 @@ $patchFile = Join-Path $script_dir "patches\go-$($env:gosrc_ver).patch"
 Set-Location (Join-Path $build_dir "go\src")
 Write-Host "Starting build"
 $env:GOROOT_BOOTSTRAP = Join-Path $build_dir "go_bootstrap"
-$env:GOHOSTARCH = "386"
+$env:GOOS = "windows"
+$env:GOARCH = "386"
 
 & .\make.bat
