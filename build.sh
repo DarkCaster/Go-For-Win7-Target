913b9eadd55588c0c935b590d0c70e26fd1fac5f
DarkCaster <dark.caster@outlook.com>
2025-07-14 07:46:50 +0300

    Added initial patch for go 1.24.5, workflow, build script

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+# set version to build if not already set
+[[ -z $gosrc_ver ]] && gosrc_ver="1.24.5"
+
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+build_dir="$script_dir/build"
+
+# create build directory
+rm -rf "$build_dir"
+mkdir -p "$build_dir"
+cd "$build_dir"
+
+# download archive with bootstrap go lang compiler
+wget -q "https://go.dev/dl/go1.22.12.linux-amd64.tar.gz"
+tar xf "go1.22.12.linux-amd64.tar.gz"
+mv go go_bootstrap
+
+# download golang sources
+wget -q "https://go.dev/dl/go${gosrc_ver}.src.tar.gz"
+tar xf "go${gosrc_ver}.src.tar.gz"
+
+# apply patch
+cd go
+patch -p1 -i "$script_dir/patches/go-${gosrc_ver}.patch"
+
+# build golang
+cd src
+export GOROOT_BOOTSTRAP="$build_dir/go_bootstrap"
+chmod 755 ./make.bash
+./make.bash
